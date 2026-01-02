ba579f960ac261200570f2cdc49b2d28533ff883
ARUN KUMAR R <arunkumarr055.ds@gmail.com>
2026-01-02 10:35:57 +0530

    Create build_db.py

---
+++ build_db.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+loader = TextLoader("ipc.txt", encoding="utf-8")
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=100
+)
+docs = splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+db = FAISS.from_documents(docs, embeddings)
+db.save_local("ipc_vector_db")
+
+print("IPC Vector DB created successfully")
